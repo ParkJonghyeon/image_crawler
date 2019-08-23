c596fc63d14e9ee5bdaf6c40fc288f0ee9d84500
Jong-hyeon Park <pjh000901@gmail.com>
2019-08-23 21:18:19 +0900

    driver_open 함수 구현 및 시스템 메시지 클래스의 추가

--- image_crawler/image_crawler.py
+++ image_crawler/image_crawler.py
@@ -1,9 +1,20 @@
 from selenium import webdriver
 from crawler_user.info import UserInfo
+from system_messages import ErrorMessage
+
+def driver_open(chrome_driver_root):
+    if chrome_driver_root == None:
+        print(ErrorMessage.DRIVER_ROOT_NOT_FOUND)
+    try:
+        return webdriver.Chrome(chrome_driver_root)
+    except:
+        print(ErrorMessage.DRIVER_CAN_NOT_OPEN)
+        return None
 
 
 def main(user):
-    driver = webdriver.Chrome(user.get_chrome_root())
+    driver = driver_open(user.get_chrome_root())
+    driver.get("https://google.com")
     
 
 if __name__ == '__main__':
---
+++ image_crawler/system_messages.py
@@ -0,0 +1,6 @@
+class ProcessingMessage:
+    None
+
+class ErrorMessage:
+    DRIVER_ROOT_NOT_FOUND = "Driver root is None!"
+    DRIVER_CAN_NOT_OPEN = "Driver can't open!"
