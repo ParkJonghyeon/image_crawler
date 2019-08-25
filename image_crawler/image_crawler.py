37631c91941d017604e5aeae3a8b650a1dffbaa6
Jong-hyeon Park <pjh000901@gmail.com>
2019-08-25 22:34:54 +0900

    입력 받은 url 양식의 http 포함 여부 체크 및 포맷팅

--- image_crawler/image_crawler.py
+++ image_crawler/image_crawler.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from crawler_user.info import UserInfo
 from system_messages import ErrorMessage
+from urllib import parse
 
+# 웹 드라이버 정상 실행 체크
 def driver_open(chrome_driver_root):
     if chrome_driver_root == None:
         print(ErrorMessage.DRIVER_ROOT_NOT_FOUND)
@@ -12,11 +14,23 @@ def driver_open(chrome_driver_root):
         return None
 
 
+# 입력 받은 url의 http 포함 여부 체크
+def url_scheme_check(target_url):
+    valid_check_url = parse.urlparse(target_url)
+    if valid_check_url.scheme is '':
+        valid_check_url = valid_check_url._replace(scheme='http')
+        return valid_check_url.geturl()
+    else:
+        return target_url
+
+
+# main 실행 부
 def main(user):
     driver = driver_open(user.get_chrome_root())
-    driver.get("https://google.com")
+    input_url = url_scheme_check(input())
+    driver.get(input_url)
     
-
+    
 if __name__ == '__main__':
     user = UserInfo()
     main(user)
