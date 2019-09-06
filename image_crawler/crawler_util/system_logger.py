16c532d8b43cb3632f438557490f4ccda3ede54b
Jong-hyeon Park <pjh000901@gmail.com>
2019-09-06 19:33:44 +0900

    SystemLogger 클래스 초기화 및 인자 수정

--- image_crawler/crawler_util/system_logger.py
+++ image_crawler/crawler_util/system_logger.py
@@ -1,7 +1,11 @@
 class SystemLogger():
 
+    def __init__(self, work_dir):
+        self.work_dir = work_dir
+
+
     def print_log(log_text):
         print(log_text)
-        log_file = open(work_dir+'\log.txt','a', encoding = "utf-8")
+        log_file = open(self.work_dir+'\log.txt','a', encoding = "utf-8")
         log_file.write(log_text)
         log_file.close()
