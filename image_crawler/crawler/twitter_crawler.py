c088ce20fbd9ad83aae643d5e0c413799c87da28
Jong-hyeon Park <pjh000901@gmail.com>
2019-09-25 23:04:40 +0900

    twitter_crawler: 동적 페이지 로드 기능 구현 및 todo 항목 추가

--- image_crawler/crawler/twitter_crawler.py
+++ image_crawler/crawler/twitter_crawler.py
@@ -5,14 +5,20 @@ class TwitterCrawler(BaseCrawler):
     def __init__(self, crawler_file_util):
         self.file_util = crawler_file_util
         self.image_save_path = self.file_util.join_dir_path(self.file_util.user.get_image_save_path(), 'twitter')
+        self.driver = None
         
 
     # Override this method
     def crawler_rule(self, input_url):
         self.driver.get(input_url)
         self.driver.implicitly_wait(15)
-        self.get_twitter_profile()
-        self.get_twitter_contents()
+
+        while True:
+            self.get_twitter_profile()
+            self.get_twitter_contents()
+            if self.can_scroll_down():
+                continue
+            break
 
 
     def get_twitter_profile(self):
@@ -24,12 +30,14 @@ class TwitterCrawler(BaseCrawler):
             profile_name = profile_name.split('@')[0]
         # == ('h2 a').text
         profile_screenname = profile_header.find_element_by_css_selector('.username.u-dir').text
-        self.artist_name = profile_name + '(' + profile_screenname + ')'
-        self.image_save_path = self.file_util.join_dir_path(self.user.get_image_save_path(), self.artist_name)
+        self.artist_name = profile_name + '_' + profile_screenname
+        self.image_save_path = self.file_util.join_dir_path(self.image_save_path, self.artist_name)
 
 
     def get_twitter_contents(self):
-        #blob 링크는 js를 사용한 영상 다운로드 방법을 시도해볼것
+        # blob 링크는 js를 사용한 영상 다운로드 방법을 시도해볼것
+        # 구조 일관성 유지를 위해 해당 함수는 선별 된 timeline_contents에 대해서 반복 작업만 수행하도록 변경
+        # html 내에서 페이지의 스크롤 및 로드 된 timeline_contents를 찾는 것은 별도의 함수로 작업
         timeline = self.driver.find_element_by_css_selector('#timeline')
         timeline_contents = timeline.find_elements_by_css_selector('.js-stream-item.stream-item.stream-item')
 
@@ -45,4 +53,11 @@ class TwitterCrawler(BaseCrawler):
                                          image_url=img.get_attribute('src') + ':orig',
                                          image_save_path= self.image_save_path)
                     print(img_info)
+
+
+    def can_scroll_down(self):
+        return False
         
+
+    def posted_within_period(self, content_seconds):
+        return True
