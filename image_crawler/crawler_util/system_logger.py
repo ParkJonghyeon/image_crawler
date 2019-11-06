2171531ea0fe4f5dffd264f0e98b13cfd8bb4211
Jong-hyeon Park <pjh000901@gmail.com>
2019-11-06 21:24:12 +0900

    오탈자 수정

--- image_crawler/crawler_util/system_logger.py
+++ image_crawler/crawler_util/system_logger.py
@@ -4,7 +4,7 @@ class SystemLogger():
         self.work_dir = work_dir
 
 
-    def print_log(log_text):
+    def print_log(self, log_text):
         print(log_text)
         log_file = open(self.work_dir+'\log.txt','a', encoding = "utf-8")
         log_file.write(log_text)
